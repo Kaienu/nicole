450352d12ec32918321333218ce6caaad4b55091
kaien <kaienu.jp@gmail.com>
2018-09-05 18:01:34 +0200

    Añadiendo Clientes.java desde local

--- src/nicole_test/Cliente.java
+++ src/nicole_test/Cliente.java
@@ -1,21 +1,86 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nicole_test;
 
-/**
- *
- * @author kaien
- */
 public class Cliente {
+	
+	private String nombre_cliente,apellidos_cliente,direccion_cliente;
+	private int telefono,id_cliente;
+	private static int count_cliente=0;
+	
+	
+	public Cliente(String nombre_cliente, String apellidos_cliente, String direccion_cliente, int telefono) {
+		this.setNombre_cliente(nombre_cliente);
+		this.setApellidos_cliente(apellidos_cliente);
+		this.setDireccion_cliente(direccion_cliente);
+		this.setTelefono(telefono);
+		this.setCount_cliente();
+		this.setId_cliente();
+		
+		
+	}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+	public String getNombre_cliente() {
+		return nombre_cliente;
+	}
+
+	public void setNombre_cliente(String nombre_cliente) {
+		this.nombre_cliente = nombre_cliente;
+	}
+
+	public String getApellidos_cliente() {
+		return apellidos_cliente;
+	}
+
+	public void setApellidos_cliente(String apellidos_cliente) {
+		this.apellidos_cliente = apellidos_cliente;
+	}
+
+	public String getDireccion_cliente() {
+		return direccion_cliente;
+	}
+
+	public void setDireccion_cliente(String direccion_cliente) {
+		this.direccion_cliente = direccion_cliente;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+	private void setId_cliente() {
+		this.id_cliente=Cliente.count_cliente;
+	}
+	
+	public int getId_cliente() {
+		return this.id_cliente;
+	}
+	
+	public void setCount_cliente() {
+		Cliente.count_cliente++;
+	}
+	
+	public int getCount_cliente() {
+		return Cliente.count_cliente;
+	}
+
+	@Override
+	public String toString() {
+		return "cliente ID " + this.getId_cliente() +
+				" || " + this.getNombre_cliente() + " " + this.getApellidos_cliente() +
+				" || " + this.getDireccion_cliente() +
+				" || " + this.getTelefono();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Cliente) {
+			return this.telefono == (((Cliente) obj) .telefono);
+		} else {
+			return false;
+		}
+	}
+
 }
