ca30bcbe860c36cf3aafcd6777622d8b19f7cdbc
kaien <kaienu.jp@gmail.com>
2018-09-12 13:32:31 +0200

    Avance 12Sept

--- src/nicole_test/Presentacion.java
+++ src/nicole_test/Presentacion.java
@@ -6,16 +6,13 @@ public class Presentacion {
         
         AccesoSQL acceso = new AccesoSQL();
         
-        acceso.listado();
+        //acceso.listado();
         
         //acceso.pruebaConexion();
-        //Cliente cli2 = new Cliente("manolete", "cantinflas", "Calle serrano, 2", 954458965);
-        //Cliente cli3 = new Cliente("Juanito", "juanolas", "Avnda de la polla-santa, 197", 968458235);
-        //acceso.nuevo(cli2);
-        //acceso.nuevo(cli3);
         
-        /*Cliente cli1 = new Cliente("Jesus","Ufano","Calle del perrito caliente 21", 954856932);
-        acceso.nuevo(cli1);*/
+        Persona cli2 = new Persona("Alba", "MaPi", "Avda de la caraja-gorda, 2", 54682465);
+        acceso.nuevo(cli2);
+        
         acceso.cerrar();
         
     }
