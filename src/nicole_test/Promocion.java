7d123d98fa326de1690f54e51f9d0e84819d8884
kaien <kaienu.jp@gmail.com>
2018-09-12 17:04:26 +0200

    Avance 12Sept

---
+++ src/nicole_test/Promocion.java
@@ -0,0 +1,60 @@
+package nicole_test;
+
+public class Promocion {
+    
+    private int idPromocion,porcenProm,denominacion;
+
+    public Promocion(int idPromocion, int porcenProm, int denominacion) {
+        this.setIdPromocion(idPromocion);
+        this.setPorcenProm(porcenProm);
+        this.setDenominacion(denominacion);
+    }
+
+    public int getDenominacion() {
+        return denominacion;
+    }
+
+    public int getIdPromocion() {
+        return idPromocion;
+    }
+
+    public int getPorcenProm() {
+        return porcenProm;
+    }
+
+    public void setDenominacion(int denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public void setIdPromocion(int idPromocion) {
+        this.idPromocion = idPromocion;
+    }
+
+    public void setPorcenProm(int porcenProm) {
+        this.porcenProm = porcenProm;
+    }
+    
+    
+    
+    @Override
+    public boolean equals(Object obj) {
+	if (obj instanceof Promocion) {
+            if (((Promocion) obj).getIdFactura() == this.getIdFactura()) {
+                return ((Promocion) obj).getIdLinea() == this.getIdLinea();
+            } else return false;
+        } else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + this.idLinea;
+        hash = 43 * hash + this.idFactura;
+        hash = 43 * hash + this.idProducto;
+        hash = 43 * hash + this.idPromocion;
+        hash = 43 * hash + this.cantidad;
+        hash = 43 * hash + this.importeLinea;
+        return hash;
+    }
+    
+}
